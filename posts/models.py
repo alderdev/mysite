8357b4da9727bf386148a874734e3687abb3ea86
allen.chang <allen@CentOS.microjet.com.tw>
2016-08-25 07:26:50 +0800

    modify post imagefield to urlfiled

--- posts/models.py
+++ posts/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 # Create your models here.
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
+
 class Post(models.Model):
     subject = models.CharField(max_length=60)
     content = models.TextField()
-    image = models.FileField(null=True, blank=True)
+    #image = models.ImageField( null=True, blank=True, height_field="height_field", width_field="width_field")
+    image = models.URLField( null=True, blank=True)
+    height_field = models.IntegerField(default=24)
+    width_field = models.IntegerField(default=24)
     timestemp = models.DateTimeField(auto_now_add=True, auto_now=False)
     modify = models.DateTimeField(auto_now_add=True, auto_now=False)
 
