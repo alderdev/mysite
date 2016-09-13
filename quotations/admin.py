db96e09a3ec7917a7e18e5975a599fd0fd482f59
allen.chang <allen@CentOS.microjet.com.tw>
2016-09-14 05:48:04 +0800

    adding quotations module

--- quotations/admin.py
+++ quotations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class QuoteDetailInline(admin.TabularInline):
+    model = models.QuoteDetail
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    inlines = [QuoteDetailInline,]
+
+admin.site.register(models.Currency)
+admin.site.register(models.QuoteHead, QuoteAdmin)
--- quotations/models.py
+++ quotations/models.py
@@ -1,1 +1,84 @@
 from django.db import models
+from django.utils import timezone
+from customers.models import Customer
+from products.models import Product
+from django.core.urlresolvers import reverse
+
+
+
+#自訂單據號碼
+class OrderNumberManager(models.Manager):
+
+    #短的月份序號如：16080001
+    def short_month_sequence(self):
+        order_date, _ = str(timezone.now()).split(' ')
+        nextNumber = self.filter(create_at__contains = order_date[:7] ).count()+1
+        order_number = nextNumber + int(order_date[2:7].replace('-',''))*10000
+        return order_number
+    #月份序號如：2016080001
+    def month_sequence(self):
+        order_date, _ = str(timezone.now()).split(' ')
+        nextNumber = self.filter(create_at__contains = order_date[:7] ).count()+1
+        order_number = nextNumber + int(order_date[:7].replace('-',''))*10000
+        return order_number
+    #月份序號如：2016080001
+    def day_sequence(self):
+        order_date, _ = str(timezone.now()).split(' ')
+        nextNumber = self.filter(create_at__contains = order_date ).count()+1
+        order_number = nextNumber + int(order_date.replace('-',''))*10000
+        return order_number
+    #短的日序號如：1608310001
+    def short_day_sequence(self):
+        order_date, _ = str(timezone.now()).split(' ')
+        nextNumber = self.filter(create_at__contains = order_date ).count()+1
+        order_number = nextNumber + int(order_date.replace('-','')[2:])*10000
+        return order_number
+
+
+
+#幣別
+class Currency(models.Model):
+    code = models.CharField(primary_key=True, max_length=3, )
+    description = models.CharField(max_length=36, null=False, blank=False)
+
+    def __str__(self):
+        return self.code
+
+
+# 報價單
+class QuoteHead(models.Model):
+    request_user = models.CharField(max_length=60, null=False, blank=False) #開單人
+    order_number = models.IntegerField(null=True, blank=True, unique=True) #報價單號
+    ord_date = models.DateField(default=timezone.now) #報價日期
+    customer = models.ForeignKey(Customer) #客戶編號
+    effective_date = models.DateField( default=timezone.now ) # 報價單有效日期
+    currency = models.ForeignKey( Currency )
+    invalid = models.BooleanField(default=False) #作廢
+    comment = models.TextField(null=True, blank=True)
+
+    create_at = models.DateTimeField(auto_now_add=True, auto_now =False)
+    modify = models.DateTimeField(auto_now_add=False, auto_now =True)
+
+    objects = OrderNumberManager()
+
+
+    def get_absolute_url(self):
+        return reverse( "quotations:detail", kwargs={"pk": self.id} )
+
+
+
+# 報價單明細
+class QuoteDetail(models.Model):
+    quotehead = models.ForeignKey(QuoteHead) #報價單編號
+    line_no = models.IntegerField() #行號
+    product = models.ForeignKey(Product) # 料號
+    unit_price = models.FloatField() # 單價
+    line_memo = models.CharField(max_length=50, blank=True, null=True) # 行備註
+    invalid = models.BooleanField(default=False) #作廢
+
+    create_at = models.DateTimeField(auto_now_add=True, auto_now =False)
+    modify = models.DateTimeField(auto_now_add=False, auto_now =True)
+
+
+    def get_absolute_url(self):
+        return reverse( "quotations:detail", kwargs={"pk": self.id} )
