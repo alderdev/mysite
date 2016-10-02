496140a1f5d9ae4650a9b120aef511dfea1e167e
allen.chang <allen@CentOS.microjet.com.tw>
2016-10-02 08:01:59 +0800

    something

--- quoted/forms.py
+++ quoted/forms.py
@@ -4,8 +4,8 @@ from .models import Order
 from customers.models import Customer
 
 class OrderCreateForm(forms.ModelForm):
-    customer = forms.ModelChoiceField(queryset = Customer.objects.all(), widget=forms.TextInput )
-
+    customer = forms.ModelChoiceField( queryset= Customer.objects.all() ,
+                                        widget= forms.TextInput( attrs={'class':'form-control' , 'onfocus':'select()', 'require':'True' } ) )
     class Meta:
         model = Order
         fields = ['customer', 'email']
