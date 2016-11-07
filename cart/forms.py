6611bd9904211d14e439d9b9908ca2aa2873cc63
ald.dev <mis@alder.com.tw>
2016-11-07 18:05:23 +0800

    somthing stage

--- cart/forms.py
+++ cart/forms.py
@@ -3,13 +3,13 @@ from django import forms
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(widget= forms.TextInput( attrs={'class':'form-control', 'size':4 , 'onfocus':'select()'}))
     price = forms.FloatField()
-    quantity1 = forms.IntegerField(required=False)
+    quantity1 = forms.IntegerField(widget= forms.TextInput( attrs={'class':'form-control', 'size':4 , 'onfocus':'select()', 'require':'False' } ))
     price1 = forms.FloatField(required=False)
-    quantity2 = forms.IntegerField(required=False)
+    quantity2 = forms.IntegerField(widget= forms.TextInput( attrs={'class':'form-control', 'size':4 , 'onfocus':'select()', 'require':'False' } ))
     price2 = forms.FloatField(required=False)
-    quantity3 = forms.IntegerField(required=False)
+    quantity3 = forms.IntegerField(widget= forms.TextInput( attrs={'class':'form-control', 'size':4 , 'onfocus':'select()', 'require':'False' } ))
     price3 = forms.FloatField(required=False)
 
     update = forms.BooleanField(required=False,
--- cart/views.py
+++ cart/views.py
@@ -6,11 +6,15 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
+    print("cart_add")
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
+    print(form)
     if form.is_valid():
+        print("is_valid")
         cd = form.cleaned_data
+
         cart.add( product=product,quantity=cd['quantity'],price=cd['price'],update_quantity=cd['update'])
 
     return redirect('quoted:product_list')
